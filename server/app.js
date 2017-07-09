04f5cd378945519b000cb5c9a9ecb08cbd0964c4
persepolis1 <electry.ba@gmail.com>
2017-07-08 21:55:38 -0400

    Add google map matrix

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const request = require('request');
 const jsonFile = require('jsonfile');
 require('dotenv').config({path: 'secrets.env'});
 const weatherApiKey = process.env.WEATHER_API_KEY;
+const mapMatrixApiKey = process.env.MAP_MATRIX_API_KEY;
 
 request(`https://api.darksky.net/forecast/${weatherApiKey}/37.8267,-122.4233`,  (error, response, body) => {
   let responseBody = JSON.parse(body);
@@ -10,4 +11,9 @@ request(`https://api.darksky.net/forecast/${weatherApiKey}/37.8267,-122.4233`,  
     console.error(err);
   })
 });
-
+request(`https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Montreal,QC&destinations=Vaudreuil+Dorion,QC&key=${mapMatrixApiKey}`, (error,response,body) => {
+  let responseBody = JSON.parse(body);
+  jsonFile.writeFile("mapMaptrix.json", responseBody, {spaces: 2},  err => {
+    console.error(err);
+  })
+});
