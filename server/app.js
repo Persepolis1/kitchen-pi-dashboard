b1b758072df6bd040b1e4f6a6c6f2a4bc37d59ab
persepolis1 <electry.ba@gmail.com>
2017-07-09 15:59:06 -0400

    socket.io

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const request = require('request');
 const jsonFile = require('jsonfile');
+const socket = require('socket.io');
 require('dotenv').config({path: 'secrets.env'});
 const weatherApiKey = process.env.WEATHER_API_KEY;
 const mapMatrixApiKey = process.env.MAP_MATRIX_API_KEY;
 
-request(`https://api.darksky.net/forecast/${weatherApiKey}/37.8267,-122.4233`,  (error, response, body) => {
+request(`https://api.darksky.net/forecast/${weatherApiKey}/45.5017,-73.5673?units=si`,  (error, response, body) => {
   let responseBody = JSON.parse(body);
   console.error(error);
   jsonFile.writeFile("data.json", responseBody, {spaces: 2},  err => {
