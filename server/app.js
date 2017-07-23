96557ed502a4585a6025a802a4e697b5cdbf9d31
persepolis1 <electry.ba@gmail.com>
2017-07-23 15:38:12 -0400

    Setup basic express

--- server/app.js
+++ server/app.js
@@ -1,9 +1,26 @@
 const weatherAPI = require('./api/weather_api');
 const googleMatrixApi = require('./api/google_api');
 const twitterApi = require('./api/twitter_api');
+const path = require('path');
 const express = require('express');
 const socket = require('socket.io');
 
-weatherAPI('data.json');
-googleMatrixApi('mapMatrix.json');
-twitterApi('twitterData.json');
+const app = express();
+const port = 1337;
+
+//weatherAPI('data.json');
+//googleMatrixApi('mapMatrix.json');
+//twitterApi('twitterData.json');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info(`### Listening to port ${port}`);
+});
