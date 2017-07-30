6267acfce99720d7e211c43d4f5c00b3435b9963
persepolis1 <electry.ba@gmail.com>
2017-07-30 01:59:57 -0400

    Start CSS work

---
+++ src/components/Clock/index.js
@@ -0,0 +1,40 @@
+import "./Clock.styl";
+import React,{Component} from "react";
+import {getDayToString , getMonthToString} from "../../helpers/util";
+
+export default class Clock extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      30000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="clock-container">
+        <div className="clock-date">
+          <h1>{getDayToString(this.state.date.getDay())}, {getMonthToString(this.state.date.getMonth())} {this.state.date.getDate()}</h1>
+        </div>
+        <div className="clock-time">
+          <h1>{this.state.date.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true })}</h1>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import "../constants/theme.styl";
-import {getDayToString , getMonthToString} from "../helpers/util";
+import "./theme.styl";
+import Clock from "./Clock/index";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <h2>The current date is {getDayToString(this.state.date.getDay())}</h2>
-        <h2>The current month is {getMonthToString(this.state.date.getMonth())}</h2>
-        <h2>The current year is : {this.state.date.getFullYear()}</h2>
+      <div className="page-container">
+        <Clock/>
       </div>
     );
   }
