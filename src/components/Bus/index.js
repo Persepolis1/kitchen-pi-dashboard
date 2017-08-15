52a87b95e78f6aa6fd557af45af0f13f18f2e564
Bobak Ansari <electry.ba@gmail.com>
2017-08-15 00:26:38 -0400

    Add placeholder for bus locations

--- src/components/Bus/index.js
+++ src/components/Bus/index.js
@@ -4,9 +4,11 @@ import {getBusWeekdayTimes, getBusWeekendTimes} from "../../helpers/util";
 
 export default class Bus extends Component{
 
+  //To be optimized
   getBusTimes(location){
     let day = new Date().getDay();
-    (day === (0 || 6)) ? getBusWeekendTimes(location,day) : getBusWeekdayTimes(location);
+    let busTimes = (day === (0 || 6)) ? getBusWeekendTimes(location,day) : getBusWeekdayTimes(location);
+    return busTimes;
   }
   render(){
     return(
@@ -15,7 +17,9 @@ export default class Bus extends Component{
         <div className="bus-time bus-first">
           <h1>{this.getBusTimes('vaudreuil')}</h1>
         </div>
-        <div className="bus-time bus-second"></div>
+        <div className="bus-time bus-second">
+          <h1>{this.getBusTimes('coteVertu')}</h1>
+        </div>
       </div>
     );
   };
