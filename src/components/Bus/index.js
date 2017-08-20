c6c362be27b949ee28b47e41bad168baf5df35ef
Bobak Ansari <electry.ba@gmail.com>
2017-08-20 16:16:57 -0400

    Implement recent bus

--- src/components/Bus/index.js
+++ src/components/Bus/index.js
@@ -1,21 +1,25 @@
 import "./bus.styl";
 import React,{Component} from "react";
-import {getBusWeekdayTimes, getBusWeekendTimes} from "../../helpers/util";
+import {getNextBuses} from "../../helpers/util";
 
 export default class Bus extends Component{
 
   //To be optimized
   getBusTimes(location){
-    let day = new Date().getDay();
-    let busTimes = (day === (0 || 6)) ? getBusWeekendTimes(location,day) : getBusWeekdayTimes(location);
-    return busTimes;
+    let busDates = getNextBuses(location);
+    return busDates.map((busDates,index) => {
+      return(
+        <h1 key={index}>{busDates}</h1>
+      );
+    });
   }
+
   render(){
     return(
       <div className="bus-container">
         <div className="bus-title"></div>
         <div className="bus-time bus-first">
-          <h1>{this.getBusTimes('vaudreuil')}</h1>
+          {this.getBusTimes('vaudreuil')}
         </div>
         <div className="bus-time bus-second">
           <h1>{this.getBusTimes('coteVertu')}</h1>
--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,30 +1,48 @@
 import {busData} from '../constants/cit';
-import {dayToString,monthToString} from "../constants/date"
+import {dayToString, monthToString} from "../constants/date";
 
 
-//Gets the weekday times for a location (ex: vaudreuil, coteVertu)
-export function getBusWeekdayTimes(location){
-  for (let i in busData){
-    if (busData[i].location === location){
-      return busData[i].weekdayTimes;
+
+function getAllBusTimes(location) {
+  let date = new Date();
+  let day = date.getDay();
+  for (let i in busData) {
+    if (busData[i].location === location) {
+      let result = (day === 0 || 6 ? busData[i].weekendTimes[day] : busData[i].weekdayTimes);
+      return result;
     }
   }
 }
 
-//Gets the weekend times of a location, 6 for saturday, 7 for sunday
-export function getBusWeekendTimes(location, day){
-  for (let i in  busData){
-    if (busData[i].location === location){
-      return busData[i].weekendTimes[day];
+function convertStringTimeToNumber(timeArray){
+  let copyArray = [...timeArray];
+  for (let i = 0 ; i < copyArray.length ; i++){
+    copyArray[i] = copyArray[i].replace(":","");
+  }
+  return copyArray;
+}
+
+export function getNextBuses(location) {
+  let busTimes = getAllBusTimes(location);
+  let busTimesParsed = convertStringTimeToNumber(busTimes);
+  let results = [];
+  let date = new Date();
+  let time = `${date.getHours()}${date.getMinutes()}`;
+  for (let i = 0; i < busTimesParsed.length; i++) {
+    if (busTimesParsed[i] - time >= 0) {
+      results.push(busTimes[i],busTimes[i + 1],busTimes[i + 2]);
+      break;
     }
   }
+  return results;
+
 }
 
 //Returns the date in a string format
-export function getDayToString(day){
+export function getDayToString(day) {
   return dayToString[day];
 }
 
-export function getMonthToString(month){
+export function getMonthToString(month) {
   return monthToString[month];
 }
