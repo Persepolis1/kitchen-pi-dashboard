ffd1538796b85cc7794bbcb0868afb8e1eda195a
persepolis1 <electry.ba@gmail.com>
2017-09-01 15:39:36 -0400

    Implement google traffic live data

--- src/components/Traffic/index.js
+++ src/components/Traffic/index.js
@@ -1,11 +1,36 @@
+import './Traffic.styl'
 import React,{Component} from "react";
 import Box from "../Box/index";
 
 export default class Traffic extends Component{
+  constructor(){
+    super();
+  }
+  componentDidMount(){
+    window.initMap = this.initMap;
+    this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyDWJLkLsNWLpDiJBPKaZdZuVLMNka0FAQU&callback=initMap');
+  }
+  initMap(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 11,
+      center: {lat: 45.49812, lng: -73.7564797}
+    });
+
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map)
+  }
+  loadJS(url){
+    let ref = document.getElementsByTagName("script")[0];
+    let script = document.createElement('script');
+    script.src = url;
+    script.async = true;
+    ref.parentNode.insertBefore(script,ref);
+  }
   render(){
     return(
-      <Box title={"Travel Times"} icon={"car"} type={"traffic"}>
-        <p>Travel times go here</p>
+      <Box title={"Travel Times"} icon={"google"} type={"traffic"}>
+        <div ref={"map"} id={"map"}>
+        </div>
       </Box>
     );
   }
