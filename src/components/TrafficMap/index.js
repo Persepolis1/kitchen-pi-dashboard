aff64576fc9f180b1fc942dac54e8188ef4f1e35
persepolis1 <electry.ba@gmail.com>
2017-09-03 01:13:21 -0400

    Implement rotating traffic data

--- src/components/TrafficMap/index.js
+++ src/components/TrafficMap/index.js
@@ -1,23 +1,39 @@
 import './TrafficMap.styl'
+import {rotatingMapData} from "../../constants/mapLocations";
 import React,{Component} from "react";
 import Box from "../Box/index";
 
 export default class TrafficMap extends Component{
-  constructor(){
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {location : "Montreal Area"}
   }
   componentDidMount(){
-    window.initMap = this.initMap;
+    window.initMap = this.initMap.bind(this);
     this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyDWJLkLsNWLpDiJBPKaZdZuVLMNka0FAQU&callback=initMap');
   }
   initMap(){
-    var map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
       zoom: 11,
-      center: {lat: 45.49812, lng: -73.7564797}
-    });
+      center: {lat: 45.49812, lng: -73.7564797},
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: false,
+      fullscreenControl: false
 
-    var trafficLayer = new google.maps.TrafficLayer();
-    trafficLayer.setMap(map)
+    });
+    const trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+    let index =  0;
+    function panMapArea(){
+      map.panTo(({lat: rotatingMapData[index].lat, lng: rotatingMapData[index].lng}));
+      map.setZoom(rotatingMapData[index].zoom);
+      this.setState({location: rotatingMapData[index].location});
+      index = ++index === rotatingMapData.length ? index = 0 : index++;
+    }
+    setInterval(panMapArea.bind(this), 10000);
   }
   loadJS(url){
     let ref = document.getElementsByTagName("script")[0];
@@ -28,7 +44,7 @@ export default class TrafficMap extends Component{
   }
   render(){
     return(
-      <Box title={"Travel Times"} icon={"google"} type={"traffic"}>
+      <Box title={`Live Traffic : ${this.state.location}`} icon={"google"} type={"traffic"}>
         <div ref={"map"} id={"map"}>
         </div>
       </Box>
---
+++ src/constants/mapLocations.js
@@ -0,0 +1,9 @@
+export const rotatingMapData = [
+  {lat: 45.4020409, lng: -74.0401627, location: "Vaudreuil-Dorion", zoom: 15},
+  {lat: 45.4043825, lng: -73.992341, location: "Vaudreuil Bridges", zoom: 14},
+  {lat: 45.4773854, lng: -73.8078952, location: "West Island", zoom: 14},
+  {lat: 45.4952385, lng: -73.6804903, location: "15-40 Exchange", zoom: 15},
+  {lat: 45.4488768, lng: -73.644477, location: "Montreal-West", zoom: 15},
+  {lat: 45.5010194, lng: -73.5719023, location: "Downtown Montreal", zoom: 16},
+  {lat: 45.49812, lng: -73.7564797, location: "Montreal Area", zoom: 11}
+];
